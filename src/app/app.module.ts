0908629cfe6d985d4b763d9e32a89bd7334fc860
alejandroiborra <alejandro.iborragil@cieep.com>
2023-01-23 17:57:11 +0100

    trabajo ionic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {ComponentsModule} from "./components/components.module";
 import {TabsComponent} from "./components/tabs/tabs.component";
 import {InicioPageModule} from "./pages/inicio/inicio.module";
+import {HeaderComponent} from "./components/header/header.component";
 
 @NgModule({
   declarations: [AppComponent, TabsComponent],
